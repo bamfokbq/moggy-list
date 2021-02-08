c7a4b3996139635ce5dbec7674eb2d372a4dfb06
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2021-02-08 12:22:10 +0100

    Changed the path to Ninja styles

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import styles from '../../styles/Ninjas.module.css';
+import styles from '../styles/Ninjas.module.css';
 
 export const getStaticProps = async () => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
